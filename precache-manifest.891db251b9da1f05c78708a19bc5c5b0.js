46df6bfdea28102b1d8a0d491b47425c126bd3b3
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-10-07 16:47:49 +0100

    Updates

---
+++ precache-manifest.891db251b9da1f05c78708a19bc5c5b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a5aa2b88f3be071ec1b30a5aba0d33",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "526241237d1b8a7a7cad",
+    "url": "/react-app/static/css/2.82c695fb.chunk.css"
+  },
+  {
+    "revision": "5d4a3b56ca15cc975f25",
+    "url": "/react-app/static/css/main.335e2e1b.chunk.css"
+  },
+  {
+    "revision": "526241237d1b8a7a7cad",
+    "url": "/react-app/static/js/2.8a834017.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-app/static/js/2.8a834017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4a3b56ca15cc975f25",
+    "url": "/react-app/static/js/main.ed9e8c9e.chunk.js"
+  },
+  {
+    "revision": "5ed18ea31b7001ec7380",
+    "url": "/react-app/static/js/runtime-main.a9224333.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
